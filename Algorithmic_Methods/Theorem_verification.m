92b3dabc89834bf3cf0903c41e96214998d491e9
lieleuler <118282616+lieleuler@users.noreply.github.com>
2024-06-11 10:31:14 -0400

    Update Theorem_verification.m

--- Algorithmic_Methods/Theorem_verification.m
+++ Algorithmic_Methods/Theorem_verification.m
@@ -21,11 +21,19 @@ function R_value = get_R_value(delta,lambda,epsilon)
 end
 
 
+% ===== function moved to another folder =====
 
 % calculate the shortest distance between two geodesic segments
 function dist_geod_to_geod = get_dist_geod_to_geod(geod_1, geod_2)
     % To be completeed
 end
+
+% ===== function moved to another folder =====
+
+
+
+
+
 
 % calculate the Hausdorff distance between the geodesic and quasi-geodesics
 function dist_Hausdorff = get_dist_Hausdorff(geod_segments, startpoint, endpoint)
