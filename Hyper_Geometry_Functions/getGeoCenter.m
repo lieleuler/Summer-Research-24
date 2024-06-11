5d76043c604af097abebe3d3d12ebef3aeaba1a8
lieleuler <118282616+lieleuler@users.noreply.github.com>
2024-06-11 10:33:57 -0400

    Create getGeoCenter.m

---
+++ Hyper_Geometry_Functions/getGeoCenter.m
@@ -0,0 +1,9 @@
+% calculate the center of the great geodesic circle given two points on it
+function geoCenter = getGeoCenter(s,t)
+    x1 = real(s);
+    y1 = imag(s);
+    x2 = real(t);
+    y2 = imag(t);
+        
+    geoCenter = (x1^2 + y1^2 - x2^2 - y2^2) / (2 * (x1 - x2));
+end
