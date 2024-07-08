53b2571ead39f2f04b9d3c17da52e2beba799db2
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-07-08 15:25:23 -0400

    Optimization + New method based on FLT rotations

--- Hyper_Geometry_Functions/get_point_along_direction.m
+++ Hyper_Geometry_Functions/get_point_along_direction.m
@@ -1,28 +1,15 @@
 
-function point = get_point_along_direction(start, angle, magnitude)
-    % Normalize angle to be in [0, 2pi)
-    angle = mod(angle, 2*pi);
-
-    if angle == 0
-        point = real(start) + 1i*(imag(start) * exp(magnitude));
-    elseif angle == pi
-        point = real(start) + 1i*(imag(start) * exp(-magnitude)); 
-    else
-        u = real(start);
-        v = imag(start);
-        if angle ~= pi/2 && angle ~= 3*pi/2
-            center_on_real_axis = -v/(tan(angle)) + u;
-            % Mirror start across center of geodesic
-            other_point = start + 2*(center_on_real_axis - u); 
-        else
-            other_point = u + v;
-        end
-        geodesic = GeodesicSegment(start, other_point);
-        % Step along geodesic by the magnitude
-        if xor(0 <= angle && angle <= pi, real(start) < real(other_point))
-            point = geodesic.travel_from_start(magnitude);
-        else
-            point = geodesic.travel_from_start(-magnitude);
-        end
+function point = get_point_along_direction(start, phi, magnitude)
+    % Define 0 degrees to be straight up in the upper half plane
+    point_at_0_degrees = real(start) + (imag(start) * exp(magnitude))*1i;
+    if mod(phi, 2*pi) == 0
+        point = point_at_0_degrees;
+        return
     end
+
+    % Perform hyperbolic rotation
+    cot_phi_over_2 = cot(phi/2);
+    point = ((real(start) - imag(start)*cot_phi_over_2)*point_at_0_degrees - abs(start)^2)...
+            /...
+            (point_at_0_degrees - ((real(start) + imag(start)*cot_phi_over_2)));
 end
