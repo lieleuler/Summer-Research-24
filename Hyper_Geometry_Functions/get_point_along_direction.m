2bc8bf034875c0fc167f1d2fb2f3a2f9c548c638
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-07-08 14:46:58 -0400

    Bugfix

--- Hyper_Geometry_Functions/get_point_along_direction.m
+++ Hyper_Geometry_Functions/get_point_along_direction.m
@@ -3,18 +3,23 @@ function point = get_point_along_direction(start, angle, magnitude)
     % Normalize angle to be in [0, 2pi)
     angle = mod(angle, 2*pi);
 
-    if angle == pi/2
+    if angle == 0
         point = real(start) + 1i*(imag(start) * exp(magnitude));
-    elseif angle == 3*pi/2
+    elseif angle == pi
         point = real(start) + 1i*(imag(start) * exp(-magnitude)); 
     else
         u = real(start);
         v = imag(start);
-        center_on_real_axis = v/(tan(pi/2 - angle)) + u;
-        mirrored_point = start + 2*(center_on_real_axis - u);
-        geodesic = GeodesicSegment(start, mirrored_point);
+        if angle ~= pi/2 && angle ~= 3*pi/2
+            center_on_real_axis = -v/(tan(angle)) + u;
+            % Mirror start across center of geodesic
+            other_point = start + 2*(center_on_real_axis - u); 
+        else
+            other_point = u + v;
+        end
+        geodesic = GeodesicSegment(start, other_point);
         % Step along geodesic by the magnitude
-        if xor(pi/2 <= angle && angle <= 3*pi/2, real(start) < real(mirrored_point))
+        if xor(0 <= angle && angle <= pi, real(start) < real(other_point))
             point = geodesic.travel_from_start(magnitude);
         else
             point = geodesic.travel_from_start(-magnitude);
