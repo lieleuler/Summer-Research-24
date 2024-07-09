ad93428a6b5913c7e7a4c7ec1c7b8a3b1e0c8f55
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-07-09 16:10:33 -0400

    Temp

--- Hausdorff_Distance/get_minimal_distance.m
+++ Hausdorff_Distance/get_minimal_distance.m
@@ -22,7 +22,7 @@ function minimal_distance = get_minimal_distance(geodesic_1,geodesic_2)
     end
     range_si = [si_1, si_2];
     
-    % calculate the range of theta based on end points of the segement
+   % calculate the range of theta based on end points of the segement
     [start_pt_2, end_pt_2] = geodesic_2.get_endpoints();
     
     theta_1 = mod(atan(imag(start_pt_2)/(real(start_pt_2)-c)), pi);
@@ -36,19 +36,48 @@ function minimal_distance = get_minimal_distance(geodesic_1,geodesic_2)
         si_root = pi - si_root;
     end
 
-    % If si_root is not within the angles bounds of geodesic_1 
-    if ~((range_si(1) < si_root) && (si_root < range_si(2))) % no critical point in the specified range_si
-        % ADD CORRECT CONDITION HERE
-    end
+    % case 1: si_root is outside angles bounds range_si
+    if si_root < range_si(1) || range_si(2) < si_root
+        % calculate projection of nodes of geodesic_2
+        [node, dismissal] = sort([start_pt_2,end_pt_2],"ComparisonMethod","real");
+        node_projection = sqrt(imag(node)^2 + real(node)^2) * 1i;
+ 
+        [lower_pt_1,higher_pt_1] = sort([start_pt_1, end_pt_1],"ComparisonMethod","abs");
+
+        % case 1a
+        if imag(node_projection) < imag(lower_pt_1) || imag(higher_pt_1) < imag(node_projection)
+            minimal_distance = min(dist_H(start_pt_1,node), dist_H(end_pt_1,node));
+
+        % case 1b
+        else
+            slope_orth_node = -1/get_tan_slope(node,c);
+            p_node = imag(node) - slope_orth_node * real(node); % where start_pt_2 is the projection of node
+            minimal_distance = dist_H(p_node,node);
+        end
     
     % Calculate the minimizing
     x_si_root = (-2*c*r*cos(si_root)^2) / (c^2 + r^2*cos(si_root)^2);
     theta_si = acos(x_si_root); % evaluate numerical value of theta at si_root
-    % If the minimizing theta is obtained outside of the angle range for 
-    % geodesic_2, then...
-    if theta_si > range_theta(2) || theta_si < range_theta(1) % theta_si outside the range_theta 
-       % ADD CORRECT CONDITION HERE
-    else % normal case 
+
+    % case 2: si_root is within angle bound range_si BUT theta_si is obtained outside of the angle range range_theta 
+    elseif  theta_si > range_theta(2) || theta_si < range_theta(1)
+        % calculate projection of nodes of geodesic_2
+        [node, dismissal] = sort([start_pt_2,end_pt_2],"ComparisonMethod","real");
+        node_projection = sqrt(imag(node)^2 + real(node)^2) * 1i;
+ 
+        [lower_pt_1,higher_pt_1] = sort([start_pt_1, end_pt_1],"ComparisonMethod","abs");
+        
+        % case 2b
+        if imag(node_projection) < imag(lower_pt_1) || imag(higher_pt_1) < imag(node_projection)
+            minimal_distance = min(dist_H(start_pt_1,node), dist_H(end_pt_1,node));
+
+        % case 2a
+        else
+            minimal_distance = dist_H(node_projection,node);
+        end
+
+    % case 3: both in
+    else 
         numerator_at_root = 2*c^2 + 2*r^2*cos(si_root)^2 + 4*c*r*cos(si_root)*(x_si_root * cos(si_root) + sqrt(1 - x_si_root^2)*sin(si_root));
         denominator_at_root = 2 * c*r*sin(2*si_root) * sqrt(1- x_si_root^2);
         minimal_distance = acosh(1 - numerator_at_root/denominator_at_root);
