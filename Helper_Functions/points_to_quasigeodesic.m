fae0ffd56009f778f9381d070c895519ef8e02c5
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-07-11 10:30:08 -0400

    Updated to work with new output structure of random walk

--- Helper_Functions/points_to_quasigeodesic.m
+++ Helper_Functions/points_to_quasigeodesic.m
@@ -1,9 +1,8 @@
 
-
-function quasi_geodesic = points_to_geodesic(points)
-    n = length(points)-1;
+function quasi_geodesic = points_to_quasigeodesic(start_points, end_points)
+    n = length(start_points);
     quasi_geodesic(1, n) = GeodesicSegment;
     for i = 1:n
-        quasi_geodesic(i) = GeodesicSegment(points(i), points(i + 1));
+        quasi_geodesic(i) = GeodesicSegment(start_points(i), end_points(i));
     end
 end
