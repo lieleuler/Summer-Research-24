730e5ee511ef8917b5643daff06c5c261f5c39d6
Finn McKibbin <fmckibb1@swarthmore.edu>
2024-07-15 14:49:15 -0400

    Compatibility with new changes

--- main.m
+++ main.m
@@ -4,29 +4,26 @@ DELTA = log(1 + sqrt(2));
 iteration_num = 1;
 
 target_length = 10;
-lambda = 5;
-eps = 1;
-step_size = 0.1;
+lambda = 8;
+eps = 3;
+step_size = 0.01;
 min_segment_splits = 1;
-
-k = 0;
+jump_chance = 0;
 
 for i = 1:iteration_num
     tic
-    points = random_walk_hyperbolic(lambda, eps, step_size, target_length, min_segment_splits);
+    [start_points, ...
+     end_points] = random_walk_hyperbolic(lambda, eps, step_size, target_length, ...
+                                             min_segment_splits, jump_chance, false);
     toc
-    num_points = length(points)
-    %if ~verify_quasigeodesic(points, lambda, eps, step_size, 20)
-        %"NOT VERIFIED"
-    %end
+    num_points = length(start_points);
 
-    tic
-    quasi_geodesic = points_to_quasigeodesic(points);
-    true_geodesic = GeodesicSegment(points(1), points(length(points)));
-    verification = verify(quasi_geodesic, true_geodesic, DELTA, lambda, eps, ceil(num_points/3));
-    toc
+    %tic
+    %quasi_geodesic = points_to_quasigeodesic(points);
+    %true_geodesic = GeodesicSegment(points(1), points(length(points)));
+    %verification = verify(quasi_geodesic, true_geodesic, DELTA, lambda, eps, ceil(num_points/3));
+    %toc
 end
 
-verification
 %points
 %bad_percent = k/iteration_num
